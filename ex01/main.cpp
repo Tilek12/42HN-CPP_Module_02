193a10323adc8a4f767165cf076b16665c38db6e
tkubanyc <tkubanyc@student.42heilbronn.de>
2024-11-22 16:50:37 +0100

    ex01: chore: Create main function

---
+++ ex01/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 16:48:43 by tkubanyc          #+#    #+#             */
+/*   Updated: 2024/11/22 16:49:49 by tkubanyc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main( void ) {
+
+	Fixed	a;
+	Fixed	const b( 10 );
+	Fixed	const c( 42.42f );
+	Fixed	const d( b );
+
+	a = Fixed( 1234.4321f );
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	return 0;
+
+}
